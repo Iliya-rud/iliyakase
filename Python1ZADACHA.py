005cb481fcf627ec33ee9a88e53c367c49ff6fc2
Iliya-rud <62443179+Iliya-rud@users.noreply.github.com>
2020-04-26 20:30:20 +0300

    Update Python1ZADACHA.py

--- Python1ZADACHA.py
+++ Python1ZADACHA.py
@@ -1,3 +1,4 @@
+import os
 print("hi")
 #Каких элементов последовательности больше: с четными значениями, или с нечетными?
 #f = open("file.txt")
@@ -5,33 +6,44 @@ print("hi")
 #for i in xrange
 even=0
 uneven=0
-r=0
+filename = "file.txt"
+def CheckFile(file):#проверка файла на наличие открытия
+    try:
+        if (os.stat(file).st_size >= 0):
+            print("File opened successfully")
+            f = open(file, "r")
+            check_string = f.read()
+            if (len(check_string) == 0):
+                print("Error, empty file")
+                raise Exception("Empty file exception")#выдаст ошибку, если файл пуст
+    except OSError:
+        print("Error: cannot open file, or file is empty")
+        raise Exception("No file in the directory")
+    f.close()
+CheckFile(filename)
 with open("file.txt") as f:#эта конструкция позволяет не беспокоиться о закрытии файла
     for d in f:
         print("d =",d)
-        r=1
         print ()
         for i in d.split(' '):#таким образом указали пробел в качестве разделителя
             b=int(i)
             print("b =",b)
             if b%2==0:
-            	even += 1
-            	print("even =",even)
+                even += 1
+                print("even =",even)
             if b%2==1:
-            	uneven += 1
-            	print("uneven =",uneven)
+                uneven += 1
+                print("uneven =",uneven)
 print("rez:")
 print("even =",even)
 print("uneven =",uneven)
-if r==0:
-    print("FILE WITHOUT NUMBERS")
-if r==1:
-    if even>uneven:
-        print("EVEN")
-    if even<uneven:
-        print("UNEVEN")
-    if even==uneven:
-        print("EQUALLY")
+print("ANSVER:")
+if even>uneven:
+    print("EVEN")
+if even<uneven:
+    print("UNEVEN")
+if even==uneven:
+    print("EQUALLY")
 #res=open("result.txt", "w")
 #res.write("")
 #res.close
